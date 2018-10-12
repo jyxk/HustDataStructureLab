b28aec74f2b6b9061231d9921bdaf639dcf92d5a
jyxk <jyxking007@gmail.com>
2018-10-12 21:21:34 +0800

    finished

--- lab01/main.c
+++ lab01/main.c
@@ -40,10 +40,28 @@ Status LoadData(PtrToSet list_set) {
     FILE *fp = fopen("SqListDataBase", "rb");
     if (fp == NULL)
         return ERROR;
-    
-    
 
+    int size = 0xffff;
+    int count = 0;
+    SqList my_list, *L = &my_list;
+    L->next = NULL;
 
+    while (1) {
+        SqList *tmp = (SqList *)malloc(sizeof(SqList));
+        size = fread(tmp, sizeof(SqList), 1, fp);
+        if (size == 0)
+            break;
+        count++;
+        tmp->data = (int *)malloc(sizeof(int) * tmp->listsize);
+        size = fread(tmp->data, sizeof(SqList), 1, fp);
+        L->next = tmp;
+        L = L->next;
+    }
+    
+    list_set->head = my_list.next;
+    list_set->num = count;
+
+    fclose(fp);
 }
 
 Status SaveData(PtrToSet list_set) {
@@ -57,6 +75,15 @@ Status SaveData(PtrToSet list_set) {
     FILE *fp = fopen("SqListDataBase", "wb");
     if (fp == NULL)
         return ERROR;
+
+    SqList *L = list_set->head;
+    while (L != NULL) {
+        fwrite(L, sizeof(SqList), 1, fp);
+        fwrite(L->data, sizeof(int) * L->listsize, 1, fp);
+        L = L->next;
+    }
+
+    fclose(fp);
 }
 
 int main(void) {
@@ -76,7 +103,7 @@ int main(void) {
         list_set.num = 0;
         LoadData(&list_set);
 
-        int list_id, list_elem, list_index;
+        int list_id, list_elem, list_index, elem_order;
         SqList *L = NULL;
 
         switch(func_flag) {
@@ -130,12 +157,309 @@ int main(void) {
                 }
                 else {
                     while (L->next != NULL) {
-                        L = L->next;
                         if (L->ListID == list_id);
                             break;
+                        L = L->next;
                     }
                 }
+                if (L->next == NULL) {
+                    printf("The list is not exist");
+                }
+                else {
+                    SqList *waiting_for_delete = L->next;
+                    L->next = L->next->next;
+                    DestroyList(waiting_for_delete);
+                    printf("destroy succeed.");
+                }
+
+                printf("\n");
+                break;
+
+            case 3:
+                printf("/*\n * Function Name: ClearList\n * Parameter: SqList *L\n * \
+                Return: Status(int)\n * Use: clear the list\n */\n");
+                printf("Then, enter the list id: ");
+                
+                scanf("%d", &list_id);
+
+                L = list_set.head;
+                while (L != NULL) {
+                    if (L->ListID == list_id) 
+                        break;
+                    L = L->next;
+                }
+                if (L == NULL) {
+                    printf("ERROR List is not exist.");
+                }
+                else {
+                    if (ClearList(L) == OK) {
+                        printf("List %d has been cleared!\n", list_id);
+                    }
+                    else {
+                        printf("Clear error, something wrong");
+                    }
+                }
+
+                printf("\n");
+                break;
+
+            case 4:
+                printf("/*\n * Function Name: IsListEmpty\n * Parameter: SqList L\n * \
+                Return: Status(int) \n * Use: to know the list is empty or not\n */\n");
+
+                printf("Then, enter the list id: ");
+                
+                scanf("%d", &list_id);
+
+                L = list_set.head;
+                while (L != NULL) {
+                    if (L->ListID == list_id) 
+                        break;
+                    L = L->next;
+                }
+                if (L == NULL) {
+                    printf("ERROR List is not exist.");
+                }
+                else {
+                    if (IsListEmpty(*L) == TRUE) {
+                        printf("List %d is empty!\n", list_id);
+                    }
+                    else {
+                        printf("List %d is not empty!\n", list_id);
+                    }
+                }
+
+                printf("\n");
+                break;
+
+            case 5:
+                printf("/*\n * Function Name: ListLength\n * Parameter: SqList L\n * \
+                Return: length of the list(int)\n * Use: get the length of the list\n */\n");
+
+                printf("Then, enter the list id: ");
+                
+                scanf("%d", &list_id);
+
+                L = list_set.head;
+                while (L != NULL) {
+                    if (L->ListID == list_id) 
+                        break;
+                    L = L->next;
+                }
+                if (L == NULL) {
+                    printf("ERROR List is not exist.");
+                }
+                else {
+                    printf("The length of the list %d is %d.\n", list_id, ListLength(*L));
+                }
+
+                printf("\n");
+                break;
+
+            case 6:
+                printf("/*\n * Function Name: GetElem\n * Parameter: SqList *L, int index, ElementType *e\n * \
+                Return: Status(int)\n * Use: get L->data[index] return it from *e\n */\n");
+
+                printf("Then, enter the list id and the index");
+                
+                scanf("%d %d", &list_id, &elem_order);
+
+                L = list_set.head;
+                while (L != NULL) {
+                    if (L->ListID == list_id) 
+                        break;
+                    L = L->next;
+                }
+                if (L == NULL) {
+                    printf("ERROR List is not exist.");
+                }
+                else {
+                    if (GetElem(L, elem_order, &list_elem) == OK) {
+                        printf("The element is %d.\n", list_elem);
+                    }
+                    else {
+                        printf("ERROR\n");
+                    }
+                }
+
+                printf("\n");
+                break;
+
+            case 7:
+                printf("/*\n * Function Name: LocateElem\n * Parameter: SqList *L, ElementType e\n * \
+                Return: the index of e or 0 if e isn't exist\n * Use: get the index of e\n */\n");
+
+                printf("Then, enter the list id and the element value");
+                
+                scanf("%d %d", &list_id, &list_elem);
+
+                L = list_set.head;
+                while (L != NULL) {
+                    if (L->ListID == list_id) 
+                        break;
+                    L = L->next;
+                }
+                if (L == NULL) {
+                    printf("ERROR List is not exist.");
+                }
+                else {
+                    list_index = LocateElem(L, list_elem);
+                    printf("The index of the %d is %d.", list_elem, list_index);
+                }
+
+                printf("\n");
+                break;
+
+            case 8:
+                printf("/*\n * Function Name: PriorElem\n * Parameter: SqList *L, ElementType cur_e, ElementType *pre_e\n * \
+                Return: Status(int)\n * Use: get the prior element of cur_e\n */\n");
+
+                printf("Then, enter the list id and the element value");
+                
+                scanf("%d %d", &list_id, &list_elem);
+
+                L = list_set.head;
+                while (L != NULL) {
+                    if (L->ListID == list_id) 
+                        break;
+                    L = L->next;
+                }
+                if (L == NULL) {
+                    printf("ERROR List is not exist.");
+                }
+                else {
+                    int elem_pre;
+                    if (PriorElem(L, list_elem, elem_pre) == OK) {
+                        printf("The piror element of %d is %d.\n", list_elem, elem_pre);
+                    }
+                    else {
+                        printf("ERROR piror is not exist~\n");
+                    }
+                }
+
+                printf("\n");
+                break;
+
+            case 9:
+                printf("/*\n * Function Name: NextElem\n * Parameter: SqList *L, ElementType cur_e, ElementType *next_e\n * \
+                Return: Status(int)\n * Use: get the next element of next_e\n */\n");
+
+                printf("Then, enter the list id and the element value");
+                
+                scanf("%d %d", &list_id, &list_elem);
+
+                L = list_set.head;
+                while (L != NULL) {
+                    if (L->ListID == list_id) 
+                        break;
+                    L = L->next;
+                }
+                if (L == NULL) {
+                    printf("ERROR List is not exist.");
+                }
+                else {
+                    int elem_next;
+                    if (NextElem(L, list_elem, &elem_next) == OK) {
+                        printf("The next element of %d is %d.\n", list_elem, &elem_next);
+                    }
+                    else {
+                        printf("ERROR next is not exist~\n");
+                    }
+                }
+
+                printf("\n");
+                break;
+
+            case 10:
+                printf("/* \n * Function Name: ListInsert\n * Parameter: SqList *l, int index, ElementType e\n * \
+                Return: Status(int)\n * Use: insert e in front of data[index]\n */\n");
+
+                printf("Then, enter the list id, index and the element value");
+                
+                scanf("%d %d %d", &list_id, &elem_order, &list_elem);
+
+                L = list_set.head;
+                while (L != NULL) {
+                    if (L->ListID == list_id) 
+                        break;
+                    L = L->next;
+                }
+                if (L == NULL) {
+                    printf("ERROR List is not exist.");
+                }
+                else {
+                    if (ListInsert(L, elem_order, list_elem) == OK)
+                        printf("Insert succeed!\n");
+                    else
+                        printf("ERROR~\n");
+                }
+
+                printf("\n");
+                break;
+
+            case 11:
+                printf("/* \n * Function Name: ListDelete\n * Parameter: SqList *L, int index, ElementType *e\n * \
+                Return: Status(int)\n * Use: delete data[index] and return its value from e\n */\n");
+
+                printf("Then, enter the list id and index");
+                
+                scanf("%d %d", &list_id, &elem_order);
+
+                L = list_set.head;
+                while (L != NULL) {
+                    if (L->ListID == list_id) 
+                        break;
+                    L = L->next;
+                }
+                if (L == NULL) {
+                    printf("ERROR List is not exist.");
+                }
+                else {
+                    if (ListDelete(L, elem_order, &list_elem) == OK)
+                        printf("Delete %d succeed!\n", list_elem);
+                    else 
+                        printf("Delete failed~\n");
+                }
+
+                printf("\n");
+                break;
+
+            case 12:
+                printf("/*\n * Function Name: ListTraverse\n * Parameter: SqList *L\n * \
+                Return: Status(int)\n * Use: traverse L\n */\n");
+
+                printf("Then, enter the list id");
+                
+                scanf("%d", &list_id);
+
+                L = list_set.head;
+                while (L != NULL) {
+                    if (L->ListID == list_id) 
+                        break;
+                    L = L->next;
+                }
+                if (L == NULL) {
+                    printf("ERROR List is not exist.");
+                }
+                else {
+                    ListTraverse(L);
+                }
+
+                printf("\n");
+                break;
+
+            case 0:
+                printf("Thank you for using~\n");
+                break;
+            
+            default:
+                printf("Baka, you entered the wrong num.\n");
+                break;
+
         }
+
+        SaveData(&list_set);
     }
+    return 0;
 }
 
