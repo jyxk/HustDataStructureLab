9bd0f0c48fb1d7358d81618e6ee1862b581c6e50
jyxk <jyxking007@gmail.com>
2018-10-19 10:32:30 +0800

    WTF what did I change

--- lab01/main.h
+++ lab01/main.h
@@ -1,3 +1,12 @@
+/*
+ * @Author: Xiuxu Jin(jyxk)
+ * @Date: 2018-10-19 09:04:01
+ * @LastEditors: Xiuxu Jin
+ * @LastEditTime: 2018-10-19 09:04:01
+ * @Description: file content
+ * @Email: jyxking007@gmail.com
+ */
+
 #ifndef _MAIN_H
 #define _MAIN_H
 
@@ -12,6 +21,7 @@ void PrintMenu(void);
      * Return: None
      * Use: print the menu 
      */
+
 
 Status LoadData(PtrToSet ListSet);
 /*
